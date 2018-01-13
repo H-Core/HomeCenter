9465c110b5dee2457fd038b6c506141aa8e1df27
HavenDV <havendv@gmail.com>
2018-01-13 16:19:19 +1000

    Added tests

---
+++ VoiceActions.NET.Tests/SpeechToTextConverters/WitAiConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using VoiceActions.NET.SpeechToTextConverters;
+using VoiceActions.NET.Tests.Utilities;
+
+namespace VoiceActions.NET.Tests.SpeechToTextConverters
+{
+    [TestFixture]
+    public class WitAiConverterTests
+    {
+        private static async Task BaseTest(string expected, byte[] data, ISpeechToTextConverter converter)
+        {
+            Assert.IsNotNull(expected);
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(converter);
+
+            Assert.AreEqual(expected, await converter.Convert(data));
+        }
+
+        [Test]
+        public async Task ConvertTest()
+        {
+            await BaseTest("проверка", TestUtilities.GetRawSpeech("speech1.wav"), new WitAiConverter("OQTI5VZ6JYDHYXTDKCDIYUODEUKH3ELS"));
+        }
+    }
+}
---
+++ VoiceActions.NET.Tests/Utilities/TestUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace VoiceActions.NET.Tests.Utilities
+{
+    public static class TestUtilities
+    {
+        public static string OutputPath => TestContext.CurrentContext.TestDirectory;
+        public static string ProjectPath => Path.GetDirectoryName(Path.GetDirectoryName(OutputPath));
+        public static string SolutionPath => Path.GetDirectoryName(ProjectPath);
+        public static string TestFilesPath => Path.Combine(ProjectPath, "TestFiles");
+        public static string RawSpeechPath => Path.Combine(TestFilesPath, "RawSpeech");
+
+        public static string GetFullPathForRawSpeech(string name) => Path.Combine(RawSpeechPath, name);
+        public static byte[] GetRawSpeech(string name) => File.ReadAllBytes(GetFullPathForRawSpeech(name));
+    }
+}
