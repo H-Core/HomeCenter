a380400e33fce22dde3190cbbe44bd47b5fa9800
HavenDV <havendv@gmail.com>
2018-01-27 01:49:52 +1000

    Added simple command class

---
+++ HomeCenter.NET/Utilities/Command.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeCenter.NET.Utilities
+{
+    public class Command
+    {
+        #region Properties
+
+        public string Data { get; set; }
+        public string Key { get; set; }
+        //public string Redirections { get; set; }
+
+        #endregion
+    }
+}
