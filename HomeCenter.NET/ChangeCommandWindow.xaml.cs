5528161fe6df75e31fed888787c273dfe9c4d0b5
HavenDV <havendv@gmail.com>
2018-01-27 01:49:24 +1000

    Added ChangeCommand Window

---
+++ HomeCenter.NET/ChangeCommandWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using HomeCenter.NET.Utilities;
+
+namespace HomeCenter.NET
+{
+    public partial class ChangeCommandWindow
+    {
+        #region Properties
+
+        public Command Command { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public ChangeCommandWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ChangeCommandWindow(Command command) : this()
+        {
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void SaveAndClose(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            Close();
+        }
+
+        private void Close(object sender, RoutedEventArgs e) => Close();
+
+        #endregion
+    }
+}
