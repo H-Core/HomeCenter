6c66cba6a9b76ebecf107a2a3d66a3e550a3ed94
HavenDV <havendv@gmail.com>
2018-01-30 21:46:41 +1000

    Added MediaUtilities.Play

---
+++ HomeCenter.NET/Utilities/MediaUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Media;
+
+namespace HomeCenter.NET.Utilities
+{
+    public static class MediaUtilities
+    {
+        public static void Play(this byte[] bytes)
+        {
+            using (var stream = new MemoryStream())
+            using (var player = new SoundPlayer(stream))
+            {
+                player.Play();
+            }
+        }
+    }
+}
