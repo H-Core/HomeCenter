f72b85baee30d2927bb1991d80ad52e137aa5855
HavenDV <havendv@gmail.com>
2018-02-11 21:48:24 +1000

    Added IStorage extension GetOrAdd

---
+++ VoiceActions.NET/Storages/StorageExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace VoiceActions.NET.Storages
+{
+    public static class StorageExtensions
+    {
+        public static T GetOrAdd<T>(this IStorage<T> storage, string key, T value = default(T))
+        {
+            if (!storage.ContainsKey(key))
+            {
+                storage[key] = value;
+            }
+
+            return storage[key];
+        }
+    }
+}
