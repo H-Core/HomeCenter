62e20d2bb8a8b83cb1ad8d91eb773f3b5c7041cb
HavenDV <havendv@gmail.com>
2018-02-11 23:41:56 +1000

    Added simple CommandControl

---
+++ HomeCenter.NET/Controls/CommandControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HomeCenter.NET.Utilities;
+
+namespace HomeCenter.NET.Controls
+{
+    public partial class CommandControl
+    {
+        #region Properties
+
+        public Command Command { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public CommandControl(Command command)
+        {
+            Command = command ?? throw new ArgumentNullException(nameof(command));
+
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void Edit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
