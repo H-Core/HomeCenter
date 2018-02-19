0f4a3a13015208235769688fce579c8fc92cbc81
HavenDV <havendv@gmail.com>
2018-02-19 14:07:11 +1000

    Added TextControl

---
+++ HomeCenter.NET/Controls/TextControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeCenter.NET.Controls
+{
+    public partial class TextControl
+    {
+        #region Properties
+
+        public string Text { get; set; }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler Deleted;
+
+        #endregion
+
+        #region Constructors
+
+        public TextControl(string text)
+        {
+            Text = text ?? string.Empty;
+
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void Delete_Click(object sender, System.Windows.RoutedEventArgs e) =>
+            Deleted?.Invoke(this, EventArgs.Empty);
+
+        #endregion
+    }
+}
