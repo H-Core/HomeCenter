4db43659d1f30afb8f72fff9668871d87b403098
HavenDV <havendv@gmail.com>
2018-04-04 00:32:27 +1000

    Added Module Extensions

---
+++ H.NET.Core/Extensions/ModuleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using H.NET.Core.Attributes;
+
+namespace H.NET.Core.Extensions
+{
+    public static class ModuleExtensions
+    {
+        public static bool AllowMultipleInstance(this Type type) => 
+            type.GetTypeInfo().GetCustomAttribute<AllowMultipleInstanceAttribute>() != null;
+
+        public static bool AllowMultipleInstance(this IModule module) => 
+            module.GetType().AllowMultipleInstance();
+
+        public static bool AutoCreateInstance(this Type type)
+        {
+            var attribute = type.GetTypeInfo().GetCustomAttribute<AllowMultipleInstanceAttribute>();
+
+            return attribute?.AutoCreateInstance ?? true;
+        }
+
+        public static bool AutoCreateInstance(this IModule module) =>
+            module.GetType().AutoCreateInstance();
+
+    }
+}
