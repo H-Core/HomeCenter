0fa0688e36c2a02b1062858b1923a14bbe129a5e
HavenDV <havendv@gmail.com>
2018-04-11 21:47:23 +1000

    Refactored Startup Utilities

--- HomeCenter.NET/Utilities/Startup.cs
+++ HomeCenter.NET/Utilities/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using H.NET.Core.Utilities;
 using Microsoft.Win32;
 
 namespace HomeCenter.NET.Utilities
@@ -11,19 +10,10 @@ namespace HomeCenter.NET.Utilities
 
         public static void Set(string fileName, bool autoStart, string arguments = null)
         {
-            if (string.IsNullOrWhiteSpace(fileName))
-            {
-                throw new ArgumentNullException(nameof(fileName));
-            }
+            var name = ToName(fileName);
 
-            var name = Path.GetFileName(fileName);
-            if (name == null)
-            {
-                throw new Exception("File name is not correct");
-            }
-
-            using (var localMachine = Registry.CurrentUser)
-            using (var key = localMachine.OpenSubKey(KeyName, true) ?? localMachine.CreateSubKey(KeyName))
+            using (var mainKey = Registry.CurrentUser)
+            using (var key = mainKey.CreateSubKey(KeyName))
             {
                 if (key == null)
                 {
@@ -42,40 +32,49 @@ namespace HomeCenter.NET.Utilities
             }
         }
 
+        public static string GetFilePath(string fileName)
+        {
+            var name = ToName(fileName);
+
+            using (var mainKey = Registry.CurrentUser)
+            using (var key = mainKey.OpenSubKey(KeyName))
+            {
+                if (!(key?.GetValue(name) is string value))
+                {
+                    return null;
+                }
+
+                return value;
+            }
+        }
+
         public static bool IsStartup(string fileName)
+        {
+            var path = GetFilePath(fileName);
+            var valueFileName = ToFileName(path);
+
+            return Compare(fileName, valueFileName);
+        }
+
+        #region Private methods
+
+        private static string ToName(string fileName)
         {
             if (string.IsNullOrWhiteSpace(fileName))
             {
                 throw new ArgumentNullException(nameof(fileName));
             }
 
-            var name = Path.GetFileName(fileName);
+            var name = Path.GetFileNameWithoutExtension(fileName);
             if (name == null)
             {
                 throw new Exception("File name is not correct");
             }
 
-            using (var localMachine = Registry.CurrentUser)
-            using (var key = localMachine.OpenSubKey(KeyName))
-            {
-                if (!(key?.GetValue(name) is string value))
-                {
-                    return false;
-                }
-
-                var valueFileName = ToFileName(value);
-                if (!Compare(fileName, valueFileName))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return name;
         }
 
-        #region Private methods
-
-        private static string ToValue(string fileName, string arguments) => $"\"{fileName ?? ""}\" {arguments ?? ""}".Trim();
+        private static string ToValue(string fileName, string arguments) => $"\"{fileName}\" {arguments}".Trim();
 
         private static string ToFileName(string value)
         {
@@ -84,7 +83,9 @@ namespace HomeCenter.NET.Utilities
                 return null;
             }
 
-            return value.Trim().SplitOnlyFirstIgnoreQuote(' ')[0];
+            var values = value.Trim().Split(new[] {'\"'}, StringSplitOptions.RemoveEmptyEntries);
+
+            return values.Length > 0 ? values[0] : null;
         }
 
         private static bool Compare(string first, string second) => 
