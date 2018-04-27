1e3985402dd115bff4c5e8b15ea71622588861b3
HavenDV <havendv@gmail.com>
2018-04-27 16:11:49 +1000

    Added CodeRunner project

---
+++ H.NET.Runners.CodeRunner/CodeRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CSScriptLibrary;
+using H.NET.Core.Runners;
+
+namespace H.NET.Runners.CodeRunner
+{
+    public class CodeRunner : Runner
+    {
+        #region Constructors
+
+        public CodeRunner()
+        {
+            AddAction("code", CodeCommand, "code");
+            //AddAction("code-from-file", CodeFromFileCommand, "path");
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void CodeCommand(string text)
+        {
+            var action = CSScript.LoadDelegate<Action>($@"
+void Action()
+{{
+{text}
+}}");
+
+            action();
+        }
+
+        #endregion
+    }
+}
