e3cf92034eaf57af84c4f4e4f6c2144154bdd18d
HavenDV <havendv@gmail.com>
2018-04-28 12:31:53 +1000

    Renamed CodeRunner to CSharpRunner

---
+++ H.NET.Runners.CSharpRunner/CSharpRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CSScriptLibrary;
+using H.NET.Core.Runners;
+
+namespace H.NET.Runners
+{
+    public class CSharpRunner : Runner
+    {
+        #region Constructors
+
+        public CSharpRunner()
+        {
+            AddAction("csharp", CSharpCommand, "code");
+            //AddAction("code-from-file", CodeFromFileCommand, "path");
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void CSharpCommand(string text)
+        {
+            var action = CSScript.LoadDelegate<Action<Action<string>>>($@"
+using System;
+using System.IO;
+
+void Action(Action<string> Say)
+{{
+{text}
+}}");
+
+            action(Say);
+        }
+
+        #endregion
+    }
+}
