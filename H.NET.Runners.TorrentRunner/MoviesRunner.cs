60b20e0e4e0097308d94654dc2d2068a6cf5648e
HavenDV <havendv@gmail.com>
2018-05-06 01:08:00 +1000

    Added MoviesRunner additional folders and torrent check

--- H.NET.Runners.TorrentRunner/MoviesRunner.cs
+++ H.NET.Runners.TorrentRunner/MoviesRunner.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using H.NET.Core.Runners;
@@ -11,6 +13,10 @@ namespace H.NET.Runners
         #region Properties
 
         private string Folder { get; set; }
+        private string Folder2 { get; set; }
+        private string Folder3 { get; set; }
+        private string MoviesExtensions { get; set; }
+        private int MaximumDistance { get; set; }
 
         #endregion
 
@@ -19,6 +25,10 @@ namespace H.NET.Runners
         public MoviesRunner()
         {
             AddSetting(nameof(Folder), o => Folder = o, NoEmpty, string.Empty, SettingType.Folder);
+            AddSetting(nameof(Folder2), o => Folder2 = o, NoEmpty, string.Empty, SettingType.Folder);
+            AddSetting(nameof(Folder3), o => Folder3 = o, NoEmpty, string.Empty, SettingType.Folder);
+            AddSetting(nameof(MoviesExtensions), o => MoviesExtensions = o, NoEmpty, "avi;mkv;mp4");
+            AddSetting(nameof(MaximumDistance), o => MaximumDistance = o, Positive, 5);
 
             AddAsyncAction("find-movie", FindMovieCommand, "name");
         }
@@ -40,22 +50,89 @@ namespace H.NET.Runners
 
             await SayAsync($"Ищу фильм {text}");
 
-            var files = Directory
-                .EnumerateFiles(Folder, "*.*", SearchOption.AllDirectories)
-                .Select(Path.GetFileNameWithoutExtension);
+            var files = GetFiles(Folder, Folder2, Folder3);
+            if (!files.Any())
+            {
+                await SayAsync("Ничего не найдено");
+                await CheckTorrent(text);
+                return;
+            }
 
             var translitedGost = Transliterator.Convert(text, Transliterator.TranslateType.Gost);
             var translitedIso = Transliterator.Convert(text, Transliterator.TranslateType.Iso);
-            Print($"TranslitedGost: {translitedGost}");
-            Print($"TranslitedIso: {translitedIso}");
-            foreach (var file in files)
+
+            var distances = new List<Tuple<int, string>>();
+            foreach (var path in files)
             {
-                Print($"File {file}. " +
-                      $"Distance: {TextUtilities.LevenshteinDistance(file, text)}. " +
-                      $"DistanceTGost: {TextUtilities.LevenshteinDistance(file, translitedGost)}. " +
-                      $"DistanceTIso: {TextUtilities.LevenshteinDistance(file, translitedIso)}");
+                distances.Add(GetDistance(path, text));
+                distances.Add(GetDistance(path, translitedGost));
+                distances.Add(GetDistance(path, translitedIso));
             }
+
+            distances = distances.OrderBy(i => i.Item1).ToList();
+
+            var minimumItem = distances.FirstOrDefault();
+            var minimumDistance = minimumItem?.Item1 ?? int.MaxValue;
+            if (minimumDistance > MaximumDistance)
+            {
+                Print($"Ближайшее совпадение: дистанция {minimumDistance} и строка {minimumItem?.Item2}");
+
+                await SayAsync("Ничего подходящего не найдено");
+                await CheckTorrent(text);
+                return;
+            }
+
+            var goodDistances = distances.Where(i => Math.Abs(i.Item1 - minimumDistance) <= 2).Distinct().ToList();
+            if (goodDistances.Count == 1)
+            {
+                await StartMovie(goodDistances[0].Item2);
+                return;
+            }
+
+            foreach (var distance in goodDistances)
+            {
+                Print($"File {distance.Item2}. Distance: {distance.Item1}");
+            }
+
         }
+
+        private async Task StartMovie(string path)
+        {
+            await SayAsync("Нашла. Запускаю");
+            Run($"start {path}");
+        }
+
+        private async Task CheckTorrent(string text)
+        {
+            await SayAsync("Скачать с торрента?");
+
+            var answer = await WaitAccept(5000, "скачай", "скачать");
+            if (!answer)
+            {
+                return;
+            }
+
+            Run($"torrent {text}");
+        }
+
+        private static Tuple<int, string> GetDistance(string path, string text)
+        {
+            var name = Path.GetFileNameWithoutExtension(path);
+            var distance = TextUtilities.MinimalLevenshteinDistance(name, text);
+
+            return new Tuple<int, string>(distance, path);
+        }
+
+        private bool IsGoodExtension(string path) => MoviesExtensions
+            .Split(';')
+            .Any(extension => string.Equals(Path.GetExtension(path), $".{extension}", StringComparison.OrdinalIgnoreCase));
+
+        private List<string> GetFiles(params string[] folders) => folders
+            .Where(folder => !string.IsNullOrWhiteSpace(folder) && Directory.Exists(folder))
+            .SelectMany(folder => Directory
+                .EnumerateFiles(folder, "*.*", SearchOption.AllDirectories)
+                .Where(IsGoodExtension))
+            .ToList();
 
         #endregion
     }
