401755860bb22423610589e69e0b3425d7b4a38b
HavenDV <havendv@gmail.com>
2018-08-04 19:00:41 +1000

    Added Core.ISearcher and Runner.Search method

---
+++ H.NET.Core/ISearcher.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace H.NET.Core
+{
+    public interface ISearcher : IModule
+    {
+        Task<List<string>> Search(string query);
+    }
+}
