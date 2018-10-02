43f060212be717f93153f946718f7871e00d896c
HavenDV <havendv@gmail.com>
2018-10-02 19:03:05 +1000

    Added User32Methods

---
+++ HomeCenter.NET/Utilities/User32Methods.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HomeCenter.NET.Utilities
+{
+    public static class User32Methods
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetForegroundWindow();
+
+        [DllImport("user32")]
+        private static extern uint GetWindowThreadProcessId(int hWnd, out int lpdwProcessId);
+
+        public static int GetWindowProcessId(int hwnd)
+        {
+            GetWindowThreadProcessId(hwnd, out var pid);
+
+            return pid;
+        }
+    }
+}
