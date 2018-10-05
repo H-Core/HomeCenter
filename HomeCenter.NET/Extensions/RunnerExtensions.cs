7a966319173c32470ebb3617b7b245040e0ab8ba
HavenDV <havendv@gmail.com>
2018-10-05 16:24:04 +1000

    Added RunnerExtensions.CheckPathAndRun

---
+++ HomeCenter.NET/Extensions/RunnerExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using H.NET.Core.Runners;
+
+namespace HomeCenter.NET.Extensions
+{
+    public static class RunnerExtensions
+    {
+        #region CheckPathAndRun
+
+        public static void CheckPathAndRun<T>(this Runner runner, string path, Func<string, T> action)
+        {
+            runner.CheckPathAndRun(path, o => { action?.Invoke(o); });
+        }
+
+        public static void CheckPathAndRun(this Runner runner, string path, Action<string> action)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                runner.Print(@"Path is empty"); //TODO: may be need to use Module.Log?
+                return;
+            }
+            if (!File.Exists(path))
+            {
+                runner.Print(@"Path is not exists");
+                return;
+            }
+
+            action?.Invoke(path);
+        }
+
+        #endregion
+    }
+}
