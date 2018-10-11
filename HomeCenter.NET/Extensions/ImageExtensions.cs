ea3411073d65d63da89ee0dcad045f103703a785
Konstantin S <havendv@gmail.com>
2018-10-11 22:59:29 +1000

    Added ImageExtensions class

---
+++ HomeCenter.NET/Extensions/ImageExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace HomeCenter.NET.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static BitmapImage ToBitmapImage(this Image image)
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = image.ToMemoryStream();
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+
+        public static MemoryStream ToMemoryStream(this Image image)
+        {
+            var stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Bmp);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return stream;
+        }
+    }
+}
