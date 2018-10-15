bede18cade391f153f5a9501f6c8b20d54ee225f
Konstantin S <havendv@gmail.com>
2018-10-15 17:59:46 +1000

    Added BooleanToColor Converters

---
+++ HomeCenter.NET/Converters/BooleanToGreenBisqueColorConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace HomeCenter.NET.Converters
+{
+    public class BooleanToGreenBisqueColorConverter : BooleanToColorConverter
+    {
+        public BooleanToGreenBisqueColorConverter() : base(Colors.LightGreen, Colors.Bisque)
+        {
+        }
+    }
+}
---
+++ HomeCenter.NET/Converters/BooleanToGreenRedColorConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace HomeCenter.NET.Converters
+{
+    public class BooleanToGreenRedColorConverter : BooleanToColorConverter
+    {
+        public BooleanToGreenRedColorConverter() : base(Colors.LightGreen, Colors.Red)
+        {
+        }
+    }
+}
