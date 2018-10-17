adc689e8a4b59d25f56bb37746f4a49b0dbf4d0f
Konstantin S <havendv@gmail.com>
2018-10-17 16:32:52 +1000

    Added ScreenExtensions

---
+++ HomeCenter.NET/Extensions/ScreenExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Caliburn.Micro;
+
+namespace HomeCenter.NET.Extensions
+{
+    // TODO: may be as IWindowManager extensions?
+    public static class ScreenExtensions
+    {
+        public static IWindowManager WindowManager => IoC.Get<IWindowManager>();
+        public static object GetInstance(Type type) => IoC.GetInstance(type, null);
+
+        public static void ShowWindow<T>(this Screen screen, IDictionary<string, object> settings = null)
+        {
+            WindowManager.ShowWindow(GetInstance(typeof(T)), null, settings);
+        }
+
+        public static bool? ShowDialog<T>(this Screen screen, IDictionary<string, object> settings = null)
+        {
+            return WindowManager.ShowDialog(GetInstance(typeof(T)), null, settings);
+        }
+
+        public static void ShowWindow(this Screen screen, Screen rootModel, object context = null, IDictionary<string, object> settings = null)
+        {
+            WindowManager.ShowWindow(rootModel, context, settings);
+        }
+
+        public static bool? ShowDialog(this Screen screen, Screen rootModel, object context = null, IDictionary<string, object> settings = null)
+        {
+            return WindowManager.ShowDialog(rootModel, context, settings);
+        }
+    }
+}
