bcb8da691023c70d31298e02228e5e94351ecfd3
Konstantin S <havendv@gmail.com>
2018-10-17 17:04:07 +1000

    Added Main View and Model

--- HomeCenter.NET/Bootstrapper.cs
+++ HomeCenter.NET/Bootstrapper.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Threading;
 using Caliburn.Micro;
-using H.NET.Storages;
+using H.NET.Core;
 using HomeCenter.NET.Properties;
 using HomeCenter.NET.Runners;
+using HomeCenter.NET.Services;
 using HomeCenter.NET.Utilities;
 using HomeCenter.NET.ViewModels;
 using HomeCenter.NET.ViewModels.Commands;
@@ -32,19 +35,46 @@ namespace HomeCenter.NET
             Container
                 .Singleton<IWindowManager, WindowManager>()
                 .Singleton<IEventAggregator, EventAggregator>()
-                .Instance(Settings.Default)
-                .Instance(new GlobalRunner(new CommandsStorage(Options.CompanyName)));
+                .Singleton<HookService>()
+                .Singleton<MainService>()
+                .Instance(Settings.Default);
 
             Container
                 .PerRequest<CommandSettingsViewModel>()
-                .PerRequest<CommandsViewModel>()
                 .PerRequest<ModuleSettingsViewModel>()
-                .PerRequest<SettingsViewModel>()
-                .Singleton<PopUpViewModel>();
+                .Singleton<CommandsViewModel>()
+                .Singleton<SettingsViewModel>()
+                .Singleton<PopUpViewModel>()
+                .Singleton<MainViewModel>();
 
             base.Configure();
 
+            #region Add Visibility Name Convection
+
             ConventionManager.AddElementConvention<UIElement>(UIElement.VisibilityProperty, "Visibility", "VisibilityChanged");
+
+            void BindVisibilityProperties(IEnumerable<FrameworkElement> frameWorkElements, Type viewModel)
+            {
+                foreach (var frameworkElement in frameWorkElements)
+                {
+                    var propertyName = frameworkElement.Name + "IsVisible";
+                    var property = viewModel.GetPropertyCaseInsensitive(propertyName);
+                    if (property == null)
+                    {
+                        continue;
+                    }
+
+                    var convention = ConventionManager
+                        .GetElementConvention(typeof(FrameworkElement));
+                    ConventionManager.SetBindingWithoutBindingOverwrite(
+                        viewModel,
+                        propertyName,
+                        property,
+                        frameworkElement,
+                        convention,
+                        convention.GetBindableProperty(frameworkElement));
+                }
+            }
 
             var baseBindProperties = ViewModelBinder.BindProperties;
             ViewModelBinder.BindProperties =
@@ -69,39 +99,124 @@ namespace HomeCenter.NET
                     // ReSharper disable once PossibleMultipleEnumeration
                     return baseBindActions(elements, viewModel);
                 };
+
+            #endregion
         }
 
-        private static void BindVisibilityProperties(IEnumerable<FrameworkElement> frameWorkElements, Type viewModel)
+        private static void Run(string command)
         {
-            foreach (var frameworkElement in frameWorkElements)
-            {
-                var propertyName = frameworkElement.Name + "IsVisible";
-                var property = viewModel.GetPropertyCaseInsensitive(propertyName);
-                if (property == null)
-                {
-                    continue;
-                }
+            var service = IoC.GetInstance(typeof(MainService), null) as MainService ?? throw new ArgumentNullException();
 
-                var convention = ConventionManager
-                    .GetElementConvention(typeof(FrameworkElement));
-                ConventionManager.SetBindingWithoutBindingOverwrite(
-                    viewModel,
-                    propertyName,
-                    property,
-                    frameworkElement,
-                    convention,
-                    convention.GetBindableProperty(frameworkElement));
-            }
+            service.Run(command);
         }
 
-        protected override void OnStartup(object sender, StartupEventArgs e)
+        private static void DisposeObject<T>() where T : class, IDisposable
         {
-            var manager = GetInstance(typeof(IWindowManager), null) as IWindowManager;
+            var obj = IoC.GetInstance(typeof(T), null) as T ?? throw new ArgumentNullException();
+
+            obj.Dispose();
+        }
+
+        protected override void OnExit(object sender, EventArgs e)
+        {
+            DisposeObject<MainViewModel>();
+            DisposeObject<MainService>();
+            DisposeObject<HookService>();
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            var manager = GetInstance(typeof(IWindowManager), null) as IWindowManager ?? throw new ArgumentNullException();
             var instance = GetInstance(typeof(PopUpViewModel), null) as PopUpViewModel ?? throw new Exception(@"PopUpViewModel Instance is null");
 
-            manager?.ShowWindow(instance);
+            // Create permanent hidden PopupView
+            manager.ShowWindow(instance);
 
-            //DisplayRootViewFor<SettingsViewModel>();
+            //DisplayRootViewFor<MainViewModel>();
+
+            var model = IoC.GetInstance(typeof(MainViewModel), null) as MainViewModel ?? throw new ArgumentNullException();
+            var mainService = IoC.GetInstance(typeof(MainService), null) as MainService ?? throw new ArgumentNullException();
+
+            //manager?.ShowWindow(IoC.GetInstance(typeof(MainViewModel), null));
+
+
+            var isKillAll = e.Args.Contains("/killall");
+            if (isKillAll)
+            {
+                Process.GetProcessesByName(Options.ApplicationName)
+                    .Where(i => i.Id != Process.GetCurrentProcess().Id)
+                    .AsParallel()
+                    .ForAll(i => i.Kill());
+            }
+
+            var isRestart = e.Args.Contains("/restart");
+
+            // If current process is not first
+            if (Process.GetProcessesByName(Options.ApplicationName).Length > 1 &&
+                !isRestart && !isKillAll)
+            {
+                Application.Shutdown();
+                return;
+            }
+
+            // Catching unhandled exceptions
+            WpfSafeActions.Initialize();
+
+            manager.ShowWindow(model);
+            // TODO: custom window manager is required
+            model.IsVisible = isRestart || !Settings.Default.IsStartMinimized;
+
+
+            var isUpdating = e.Args.Contains("/updating");
+
+            #region Static Runners
+
+            var staticRunners = new List<IRunner>
+            {
+                new DefaultRunner(model.Print, model.Say, model.Search),
+                new KeyboardRunner(),
+                new WindowsRunner(),
+                new ClipboardRunner
+                {
+                    ClipboardAction = command => Application.Current.Dispatcher.Invoke(() => Clipboard.SetText(command)),
+                    ClipboardFunc = () => Application.Current.Dispatcher.Invoke(Clipboard.GetText)
+                },
+                new UiRunner
+                {
+                    // TODO: refactor
+                    RestartAction = command => Application.Current.Dispatcher.Invoke(() => mainService.Restart(command)),
+                    UpdateRestartAction = command => Application.Current.Dispatcher.Invoke(() => mainService.RestartWithUpdate(command)),
+                    ShowUiAction = () => Application.Current.Dispatcher.Invoke(() => model.IsVisible = !model.IsVisible),
+                    ShowSettingsAction = () => Application.Current.Dispatcher.Invoke(() => model.ShowSettings()),
+                    ShowCommandsAction = () => Application.Current.Dispatcher.Invoke(() => model.ShowCommands()),
+                    ShowModuleSettingsAction = name => Application.Current.Dispatcher.Invoke(() => model.ShowModuleSettings(name)),
+                    StartRecordAction = timeout => Application.Current.Dispatcher.Invoke(() => mainService.StartRecord(timeout))
+                }
+            };
+            foreach (var runner in staticRunners)
+            {
+                ModuleManager.Instance.AddStaticInstance(runner.ShortName, runner);
+            }
+
+            #endregion
+
+            await model.Load(isUpdating);
+
+            if (!isUpdating && Settings.Default.AutoUpdateAssemblies)
+            {
+                Run("update-assemblies");
+            }
+            if (e.Args.Contains("/run"))
+            {
+                var commandIndex = e.Args.ToList().IndexOf("/run") + 1;
+                var text = e.Args[commandIndex].Trim('"');
+                var commands = text.Split(';');
+
+                foreach (var command in commands)
+                {
+                    Run(command);
+                }
+            }
         }
 
         protected override object GetInstance(Type service, string key)
@@ -122,7 +237,11 @@ namespace HomeCenter.NET
         protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
-            MessageBox.Show(e.Exception.Message, "An error as occurred", MessageBoxButton.OK);
+#if DEBUG
+            MessageBox.Show(e.Exception.ToString(), "An error as occurred", MessageBoxButton.OK, MessageBoxImage.Error);
+#else           
+            MessageBox.Show(e.Exception.Message, "An error as occurred", MessageBoxButton.OK, MessageBoxImage.Error); 
+#endif
         }
     }
 }
---
+++ HomeCenter.NET/ViewModels/MainViewModel.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+using Caliburn.Micro;
+using H.NET.Core;
+using H.NET.Core.Runners;
+using H.NET.Plugins;
+using H.NET.Utilities;
+using HomeCenter.NET.Extensions;
+using HomeCenter.NET.Services;
+using HomeCenter.NET.Utilities;
+using HomeCenter.NET.ViewModels.Commands;
+using HomeCenter.NET.ViewModels.Modules;
+using HomeCenter.NET.ViewModels.Settings;
+
+namespace HomeCenter.NET.ViewModels
+{
+    internal class MainViewModel : Screen, IDisposable
+    {
+        #region Properties
+
+        public IWindowManager WindowManager { get; }
+        public MainService MainService { get; }
+        public HookService HookService { get; }
+        public Properties.Settings Settings { get; }
+        public PopUpViewModel PopUpViewModel { get; }
+
+        private string _text;
+        public string Text {
+            get => _text;
+            set {
+                _text = value;
+                NotifyOfPropertyChange(nameof(Text));
+            }
+        }
+
+        private string _input;
+        public string Input {
+            get => _input;
+            set {
+                _input = value;
+                NotifyOfPropertyChange(nameof(Input));
+            }
+        }
+
+        private bool _isVisible;
+        public bool IsVisible {
+            get => _isVisible;
+            set {
+                _isVisible = value;
+                NotifyOfPropertyChange(nameof(IsVisible));
+            }
+        }
+
+        private bool _isRecord;
+        public bool IsRecord {
+            get => _isRecord;
+            set {
+                _isRecord = value;
+                NotifyOfPropertyChange(nameof(IsRecord));
+            }
+        }
+
+        private bool UserCanClose { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel(IWindowManager windowManager, Properties.Settings settings, PopUpViewModel popUpViewModel, MainService mainService, HookService hookService)
+        {
+            WindowManager = windowManager ?? throw new ArgumentNullException(nameof(windowManager));
+            Settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            PopUpViewModel = popUpViewModel ?? throw new ArgumentNullException(nameof(popUpViewModel));
+            MainService = mainService ?? throw new ArgumentNullException(nameof(mainService));
+            HookService = hookService ?? throw new ArgumentNullException(nameof(hookService));
+
+            MainService.GlobalRunner.NewOutput += Print;
+            MainService.Manager.Started += (sender, args) =>
+            {
+                IsRecord = true;
+                MainService.HiddenRun("deskband start");
+            };
+            MainService.Manager.Stopped += (sender, args) =>
+            {
+                IsRecord = false;
+                MainService.HiddenRun("deskband stop");
+            };
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Print(string text)
+        {
+            Text += $"{DateTime.Now:T}: {text}{Environment.NewLine}";
+
+            if (Settings.EnablePopUpMessages)
+            {
+                PopUpViewModel.Show(text, 5000);
+            }
+        }
+
+        public async Task Say(string text)
+        {
+            var synthesizer = Options.Synthesizer;
+            if (synthesizer == null)
+            {
+                Print("Synthesizer is not found");
+                return;
+            }
+
+            var bytes = await synthesizer.Convert(text);
+
+            await bytes.PlayAsync();
+        }
+
+        public async Task<List<string>> Search(string text)
+        {
+            var searcher = Options.Searcher;
+            if (searcher == null)
+            {
+                Print("Searcher is not found");
+                return new List<string>();
+            }
+
+            return await searcher.Search(text);
+        }
+
+        public void Record()
+        {
+            MainService.StartRecord(3000);
+        }
+
+        public void ShowCommands()
+        {
+            this.ShowWindow<CommandsViewModel>();
+        }
+
+        public void ShowSettings()
+        {
+            this.ShowWindow<SettingsViewModel>();
+        }
+
+        public void ShowModuleSettings(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Print("ShowModuleSettings: Module name is empty");
+                return;
+            }
+
+            var module = ModuleManager.Instance.GetPlugin<IModule>(name)?.Value;
+            if (module == null)
+            {
+                Print($"ShowModuleSettings: Module {name} is not found");
+                return;
+            }
+
+            this.ShowWindow(new ModuleSettingsViewModel(module));
+        }
+
+        public async Task Load(bool isUpdating = false)
+        {
+            #region Modules
+
+            AssembliesManager.LogAction = Print;
+            Module.LogAction = Print;
+            Runner.SearchFunc = Search;
+
+            Print("Loading modules...");
+            try
+            {
+                await MainService.Load();
+
+                //HookService.KeyboardHook.SetEnabled(Settings.EnableKeyboardHook);
+                //HookService.MouseHook.SetEnabled(Settings.EnableMouseHook);
+
+                Print("Loaded");
+            }
+            catch (Exception exception)
+            {
+                Print(exception.ToString());
+            }
+
+            #endregion
+
+            #region Hook
+
+            try
+            {
+                HookService.KeyboardHook.KeyUp += Global_KeyUp;
+                HookService.KeyboardHook.KeyDown += Global_KeyDown;
+
+                //ScreenshotRectangle.ActivationKeys.Add(Key.Space);
+                //ScreenshotRectangle.ActivationModifiers.Add(ModifierKeys.Shift);
+                //ScreenshotRectangle.NewImage += image => Clipboard.SetImage(image.ToBitmapImage());
+                //HookService.MouseHook.MouseUp += ScreenshotRectangle.Global_MouseUp;
+                //HookService.MouseHook.MouseDown += ScreenshotRectangle.Global_MouseDown;
+                //HookService.MouseHook.MouseMove += ScreenshotRectangle.Global_MouseMove;
+
+                HookService.MouseHook.MouseDown += Global_MouseDown;
+            }
+            catch (Exception exception)
+            {
+                Print(exception.ToString());
+            }
+
+            #endregion
+        }
+
+        public void Input_KeyUp(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    if (Input.Length == 0)
+                    {
+                        break;
+                    }
+                    if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                    {
+                        Input += Environment.NewLine;
+                        //InputTextBox.CaretIndex = InputTextBox.Text.Length - 1;
+                        break;
+                    }
+
+                    MainService.Run(Input);
+                    Input = string.Empty;
+                    break;
+
+                case Key.Up:
+                    if (MainService.GlobalRunner.History.Any())
+                    {
+                        Input = MainService.GlobalRunner.History.LastOrDefault() ?? "";
+                        MainService.GlobalRunner.History.RemoveAt(MainService.GlobalRunner.History.Count - 1);
+                    }
+                    break;
+            }
+        }
+
+        private void Global_KeyUp(object sender, KeyboardHookEventArgs e)
+        {
+            if (e.Key != Keys.None && e.Key == Options.RecordKey ||
+                e.IsAltPressed && e.IsCtrlPressed)
+            {
+                MainService.Manager.Stop();
+            }
+        }
+
+        private void Global_KeyDown(object sender, KeyboardHookEventArgs e)
+        {
+            if (e.Key != Keys.None && e.Key == Options.RecordKey ||
+                e.Key == Keys.Space && e.IsAltPressed && e.IsCtrlPressed)
+            {
+                MainService.Manager.Start();
+            }
+
+            if (Options.IsIgnoredApplication())
+            {
+                return;
+            }
+
+            //Print($"{e.Key:G}");
+            var combination = new KeysCombination(e.Key, e.IsCtrlPressed, e.IsShiftPressed, e.IsAltPressed);
+            if (MainService.RunCombination(combination))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Global_MouseDown(object sender, MouseEventExtArgs e)
+        {
+            if (e.SpecialButton == 0)
+            {
+                return;
+            }
+            if (Options.IsIgnoredApplication())
+            {
+                return;
+            }
+
+            var combination = KeysCombination.FromSpecialData(e.SpecialButton);
+            if (MainService.RunCombination(combination))
+            {
+                e.Handled = true;
+            }
+            //Print($"{e.SpecialButton}");
+        }
+
+        public void OnKeyUp(KeyEventArgs e)
+        {
+            // TODO: to record key
+            if (e.Key == Key.OemTilde)
+            {
+                e.Handled = true;
+            }
+        }
+
+        public void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.OemTilde)
+            {
+                e.Handled = true;
+            }
+
+            // TODO: To global?
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.R))
+            {
+                e.Handled = true;
+
+                MainService.Restart();
+            }
+        }
+
+        public void Close()
+        {
+            UserCanClose = true;
+            TryClose();
+        }
+
+        public void Dispose()
+        {
+            /* TODO: fix
+            if (TaskbarIcon != null)
+            {
+                TaskbarIcon.Icon = null;
+                TaskbarIcon.Visibility = Visibility.Hidden;
+                TaskbarIcon.Dispose();
+                TaskbarIcon = null;
+            }*/
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        public void OnClosing(CancelEventArgs e)
+        {
+            if (UserCanClose)
+            {
+                PopUpViewModel.TryClose();
+                return;
+            }
+
+            IsVisible = false;
+            e.Cancel = true;
+        }
+
+        #endregion
+    }
+}
