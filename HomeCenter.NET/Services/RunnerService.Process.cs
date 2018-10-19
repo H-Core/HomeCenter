4e3fbf49f0de5f4e21c64a29e0da6c423d196d65
Konstantin S <havendv@gmail.com>
2018-10-19 14:30:52 +1000

    Added RunnerService.Process.cs

---
+++ HomeCenter.NET/Services/RunnerService.Process.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using H.NET.Core.Runners;
+
+namespace HomeCenter.NET.Services
+{
+    public partial class RunnerService
+    {
+        public class Process
+        {
+            public bool IsCompleted { get; set; }
+            public bool IsCanceled { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Thread Thread { get; set; }
+            public Task Task { get; set; }
+            public RunInformation Information { get; set; }
+
+            public Process(string name, Task task, Thread thread)
+            {
+                Name = name;
+                Task = task;
+                Id = task.Id;
+                Thread = thread;
+            }
+
+            public Process(string name, Exception exception)
+            {
+                Name = name;
+                Information = new RunInformation(exception);
+            }
+
+            public void Cancel()
+            {
+                IsCanceled = true;
+                IsCompleted = true;
+
+                Thread.Abort();
+            }
+        }
+    }
+}
--- HomeCenter.NET/Services/RunnerService.cs
+++ HomeCenter.NET/Services/RunnerService.cs
@@ -12,41 +12,8 @@ using HomeCenter.NET.Utilities;
 
 namespace HomeCenter.NET.Services
 {
-    public class RunnerService
+    public partial class RunnerService
     {
-        public class Process
-        {
-            public bool IsCompleted { get; set; }
-            public bool IsCanceled { get; set; }
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public Thread Thread { get; set; }
-            public Task Task { get; set; }
-            public RunInformation Information { get; set; }
-
-            public Process(string name, Task task, Thread thread)
-            {
-                Name = name;
-                Task = task;
-                Id = task.Id;
-                Thread = thread;
-            }
-
-            public Process(string name, Exception exception)
-            {
-                Name = name;
-                Information = new RunInformation(exception);
-            }
-
-            public void Cancel()
-            {
-                IsCanceled = true;
-                IsCompleted = true;
-
-                Thread.Abort();
-            }
-        }
-
         #region Properties
 
         public ModuleService ModuleService { get; }
