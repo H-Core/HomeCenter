312460970a706189cb966a8acf658d65c9710705
Konstantin S <havendv@gmail.com>
2018-10-24 00:58:09 +1000

    Fixed git popup renaming bug 2

---
+++ HomeCenter.NET/Views/Utilities/PopupView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace HomeCenter.NET.Views.Utilities
+{
+    public partial class PopupView
+    {
+        public PopupView()
+        {
+            InitializeComponent();
+
+            MaxWidth = SystemParameters.WorkArea.Width / 4;
+            MaxHeight = SystemParameters.WorkArea.Height / 2;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Left = SystemParameters.WorkArea.Width - ActualWidth;
+            Top = SystemParameters.WorkArea.Height - ActualHeight;
+        }
+    }
+}
