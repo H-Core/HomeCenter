2f0de27fc13d7196e77aab8d647e10337323ef2c
Konstantin S <havendv@gmail.com>
2018-11-21 00:15:36 +1000

    Added TcpPingNotifier.OnlyIfNetworkActive property

--- Notifiers/TcpPingNotifier/TcpPingNotifier.cs
+++ Notifiers/TcpPingNotifier/TcpPingNotifier.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 namespace H.NET.Notifiers
@@ -9,6 +11,7 @@ namespace H.NET.Notifiers
 
         private string Ip { get; set; }
         private int Port { get; set; }
+        private bool OnlyIfNetworkActive { get; set; }
 
         #endregion
 
@@ -18,16 +21,29 @@ namespace H.NET.Notifiers
         {
             AddSetting(nameof(Ip), o => Ip = o, o => true, string.Empty);
             AddSetting(nameof(Port), o => Port = o, o => true, 0);
+            AddSetting(nameof(OnlyIfNetworkActive), o => OnlyIfNetworkActive = o, o => true, true);
         }
 
         #endregion
 
         #region Protected methods
 
+        public static bool IsAvailableNetworkActive()
+        {
+            return NetworkInterface.GetIsNetworkAvailable() &&
+                   (from face in NetworkInterface.GetAllNetworkInterfaces()
+                    where face.OperationalStatus == OperationalStatus.Up
+                    where (face.NetworkInterfaceType != NetworkInterfaceType.Tunnel) && (face.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                    select face.GetIPv4Statistics()).Any(statistics => (statistics.BytesReceived > 0) && (statistics.BytesSent > 0));
+        }
+
         protected override void OnElapsed()
         {
-            // TODO: Check Internet Is Available
             // TODO: Separate Runner?
+            if (OnlyIfNetworkActive && !IsAvailableNetworkActive())
+            {
+                return;
+            }
 
             try
             {
