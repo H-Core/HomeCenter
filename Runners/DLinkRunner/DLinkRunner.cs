fe527216c771a284f62d274e3b7830861d929d0a
Konstantin S <havendv@gmail.com>
2018-11-22 19:38:31 +1000

    Added DLinkRunner and reload-router command

---
+++ Runners/DLinkRunner/DLinkRunner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using H.NET.Core.Runners;
+
+namespace DLinkRunner
+{
+    // ReSharper disable once UnusedMember.Global
+    public class DLinkRunner : Runner
+    {
+        #region Properties
+
+        public string Url { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public DLinkRunner()
+        {
+            AddSetting(nameof(Url), o => Url = o, null, "http://192.168.0.1/");
+            AddSetting(nameof(Login), o => Login = o, null, "admin");
+            AddSetting(nameof(Password), o => Password = o, null, string.Empty);
+
+            AddAsyncAction("reload-router", ReloadRouter);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private async Task ReloadRouter(string text)
+        {
+            var uri = new Uri(Url);
+
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(uri, new Cookie("user_ip", "192.168.0.94"));
+            cookieContainer.Add(uri, new Cookie("cookie_lang", "rus"));
+            cookieContainer.Add(uri, new Cookie("url_hash", ""));
+            cookieContainer.Add(uri, new Cookie("client_login", Login));
+            cookieContainer.Add(uri, new Cookie("client_password", Password));
+
+            using (var client = new HttpClient(new HttpClientHandler
+            {
+                CookieContainer = cookieContainer
+            })
+            {
+                BaseAddress = uri
+            })
+            using (var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("index.cgi?res_cmd=6&res_buf=null&res_cmd_type=nbl&v2=y&rq=y&proxy=y&_=1542876693187", UriKind.Relative),
+                Method = HttpMethod.Get
+            })
+            using (var response = await client.SendAsync(request))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    Print($"Bad Response: {response}");
+                    return;
+                }
+
+                Print("Reloading in process");
+            }
+        }
+
+        #endregion
+    }
+}
