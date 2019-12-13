ecadfa1ccc4b26d01a2368f7abea10333a5a5ea5
Konstantin S <havendv@gmail.com>
2019-12-14 05:58:30 +1000

    Added YandexConverter

---
+++ Converters/YandexConverter/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Yandex.Cloud.Ai.Stt.V2;
+
+namespace YandexConverter
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            var test = SttService.Descriptor;
+        }
+    }
+}
