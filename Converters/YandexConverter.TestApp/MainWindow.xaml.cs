236315b82f7aa4ed092c195a74c5b97236010d06
Konstantin S <havendv@gmail.com>
2019-12-26 20:58:38 +1000

    YandexConverter.TestApp: Fixed UI bugs

--- Converters/YandexConverter.TestApp/MainWindow.xaml.cs
+++ Converters/YandexConverter.TestApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Windows;
 using H.NET.Recorders;
 
 namespace H.NET.Converters.TestApp
@@ -14,37 +15,53 @@ namespace H.NET.Converters.TestApp
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            using var converter = new YandexConverter
-            {
-                OAuthToken = OAuthTokenTextBox.Text,
-                FolderId = FolderIdTextBox.Text,
-                Lang = "ru-RU",
-                SampleRateHertz = 8000,
-            };
+            ProcessButton.IsEnabled = false;
+            OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} Started {Environment.NewLine}";
 
-            var recognition = await converter.StartStreamingRecognitionAsync().ConfigureAwait(false);
-            recognition.AfterPartialResults += (o, args) => Dispatcher?.Invoke(() =>
+            try
             {
-                OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} {args.Text}{Environment.NewLine}";
-                OutputTextBlock.Text = $"{DateTime.Now:h:mm:ss.fff} {args.Text}";
+                using var converter = new YandexConverter
+                {
+                    OAuthToken = OAuthTokenTextBox.Text,
+                    FolderId = FolderIdTextBox.Text,
+                    Lang = "ru-RU",
+                    SampleRateHertz = 8000,
+                };
+
+                var recognition = await converter.StartStreamingRecognitionAsync().ConfigureAwait(false);
+                recognition.AfterPartialResults += (o, args) => Dispatcher?.Invoke(() =>
+                {
+                    OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} {args.Text}{Environment.NewLine}";
+                    OutputTextBlock.Text = $"{DateTime.Now:h:mm:ss.fff} {args.Text}";
+                });
+                recognition.AfterFinalResults += (o, args) => Dispatcher?.Invoke(() =>
+                {
+                    OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} {args.Text}{Environment.NewLine}";
+                    OutputTextBlock.Text = $"{DateTime.Now:h:mm:ss.fff} {args.Text}";
+                });
+
+                using var recorder = new NAudioRecorder();
+
+                recorder.NewData += async (o, args) => await recognition.WriteAsync(args.Buffer).ConfigureAwait(false);
+                recorder.Start();
+
+                await Task.Delay(TimeSpan.FromMilliseconds(5000)).ConfigureAwait(false);
+
+                recorder.Stop();
+                await recognition.StopAsync().ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+
+            Dispatcher?.Invoke(() =>
+            {
+                ProcessButton.IsEnabled = true;
+                OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} Ended {Environment.NewLine}";
             });
-            recognition.AfterFinalResults += (o, args) => Dispatcher?.Invoke(() =>
-            {
-                OutputTextBox.Text += $"{DateTime.Now:h:mm:ss.fff} {args.Text}{Environment.NewLine}----------------------------";
-                OutputTextBlock.Text = $"{DateTime.Now:h:mm:ss.fff} {args.Text}";
-            });
-
-            using var recorder = new NAudioRecorder();
-
-            recorder.NewData += async (o, args) => await recognition.WriteAsync(args.Buffer).ConfigureAwait(false);
-            recorder.Start();
-
-            await Task.Delay(TimeSpan.FromMilliseconds(5000)).ConfigureAwait(false);
-
-            recorder.Stop();
-            await recognition.StopAsync().ConfigureAwait(false);
         }
     }
 }
