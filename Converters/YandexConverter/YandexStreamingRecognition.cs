2d0894350f63687a9b6d994bab84b1220ab6e2a8
Konstantin S <havendv@gmail.com>
2019-12-27 11:57:11 +1000

    Added WriteQueue for YandexStreamingRecognition

--- Converters/YandexConverter/YandexStreamingRecognition.cs
+++ Converters/YandexConverter/YandexStreamingRecognition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -10,12 +11,15 @@ using Yandex.Cloud.Ai.Stt.V2;
 
 namespace H.NET.Converters
 {
-    public sealed class YandexStreamingRecognition : StreamingRecognition, IDisposable
+    public sealed class YandexStreamingRecognition : StreamingRecognition
     {
         #region Properties
 
         private AsyncDuplexStreamingCall<StreamingRecognitionRequest, StreamingRecognitionResponse> Call { get; }
+
+        private ConcurrentQueue<byte[]> WriteQueue { get; } = new ConcurrentQueue<byte[]>();
         private Task ReceiveTask { get; }
+        private Task WriteTask { get; }
         private bool IsFinished { get; set; }
 
         #endregion
@@ -55,33 +59,48 @@ namespace H.NET.Converters
                     Trace.WriteLine($"{DateTime.Now:h:mm:ss.fff} YandexStreamingRecognition: {response}");
                 }
             });
+            WriteTask = Task.Run(async () =>
+            {
+                while (!IsFinished)
+                {
+                    if (!WriteQueue.TryDequeue(out var bytes))
+                    {
+                        await Task.Delay(TimeSpan.FromMilliseconds(1));
+                        continue;
+                    }
+
+                    await Call.RequestStream.WriteAsync(new StreamingRecognitionRequest
+                    {
+                        AudioContent = ByteString.CopyFrom(bytes, 0, bytes.Length),
+                    }).ConfigureAwait(false);
+                }
+            });
         }
 
         #endregion
 
         #region Public methods
 
-        public override async Task WriteAsync(byte[] bytes, CancellationToken cancellationToken = default)
+        public override Task WriteAsync(byte[] bytes, CancellationToken cancellationToken = default)
         {
-            await Call.RequestStream.WriteAsync(new StreamingRecognitionRequest
-            {
-                AudioContent = ByteString.CopyFrom(bytes, 0, bytes.Length),
-            }).ConfigureAwait(false);
+            WriteQueue.Enqueue(bytes);
+
+            return Task.CompletedTask;
         }
 
         public override async Task StopAsync(CancellationToken cancellationToken = default)
         {
+            await WriteTask;
+
             await Call.RequestStream.CompleteAsync().ConfigureAwait(false);
 
-            while (!IsFinished)
-            {
-                await Task.Delay(TimeSpan.FromMilliseconds(1), cancellationToken);
-            }
+            await ReceiveTask;
         }
 
         public void Dispose()
         {
             ReceiveTask?.Dispose();
+            WriteTask?.Dispose();
             Call?.Dispose();
         }
 
