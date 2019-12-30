292280d5e3acf556d8eb6c908b012866cdb0dc16
Konstantin S <havendv@gmail.com>
2019-12-31 00:48:11 +1000

    Fixed ConverterTests

--- H.NET.Tests/ConvertersTests.cs
+++ H.NET.Tests/ConvertersTests.cs
@@ -1,15 +1,18 @@
-﻿namespace H.NET.Tests
+﻿using System.Threading.Tasks;
+using H.NET.Converters;
+using H.NET.Tests.Utilities;
+using Xunit;
+
+namespace H.NET.Tests
 {
     public class ConvertersTests : BaseTests
     {
-        /*
         [Fact]
         public async Task WitAiConverterTest() => await BaseConverterTest(
-            "проверка", TestUtilities.GetRawSpeech("speech1.wav"), 
+            "проверка", ResourcesUtilities.ReadFileAsBytes("speech1.wav"), 
             new WitAiConverter
             {
                 Token = "OQTI5VZ6JYDHYXTDKCDIYUODEUKH3ELS"
             });
-        */
     }
 }
