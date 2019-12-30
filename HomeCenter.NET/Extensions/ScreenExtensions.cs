64ce24a256805b810221aafcfb0967c2fae91753
Konstantin S <havendv@gmail.com>
2019-12-30 21:33:46 +1000

    Fixed Extensions nullable warnings

--- HomeCenter.NET/Extensions/ScreenExtensions.cs
+++ HomeCenter.NET/Extensions/ScreenExtensions.cs
@@ -12,7 +12,7 @@ namespace HomeCenter.NET.Extensions
         public static IWindowManager WindowManager => IoC.Get<IWindowManager>();
         public static object GetInstance(Type type) => IoC.GetInstance(type, null);
 
-        public static async Task ShowWindowAsync<T>(this Screen screen, IDictionary<string, object> settings = null)
+        public static async Task ShowWindowAsync<T>(this Screen _, IDictionary<string, object>? settings = null)
         {
             var instance = GetInstance(typeof(T));
             if (instance == null)
@@ -23,7 +23,7 @@ namespace HomeCenter.NET.Extensions
             await WindowManager.ShowWindowAsync(instance, null, settings);
         }
 
-        public static async Task<bool?> ShowDialogAsync<T>(this Screen screen, IDictionary<string, object> settings = null)
+        public static async Task<bool?> ShowDialogAsync<T>(this Screen _, IDictionary<string, object>? settings = null)
         {
             var instance = GetInstance(typeof(T));
             if (instance == null)
@@ -34,17 +34,17 @@ namespace HomeCenter.NET.Extensions
             return await WindowManager.ShowDialogAsync(instance, null, settings);
         }
 
-        public static async Task ShowWindowAsync(this Screen screen, Screen rootModel, object context = null, IDictionary<string, object> settings = null)
+        public static async Task ShowWindowAsync(this Screen _, Screen rootModel, object? context = null, IDictionary<string, object>? settings = null)
         {
             await WindowManager.ShowWindowAsync(rootModel, context, settings);
         }
 
-        public static async Task<bool?> ShowDialogAsync(this Screen screen, Screen rootModel, object context = null, IDictionary<string, object> settings = null)
+        public static async Task<bool?> ShowDialogAsync(this Screen _, Screen rootModel, object? context = null, IDictionary<string, object>? settings = null)
         {
             return await WindowManager.ShowDialogAsync(rootModel, context, settings);
         }
 
-        public static async Task ShowMessageBoxAsync(this Screen screen, string text, string title = null)
+        public static async Task ShowMessageBoxAsync(this Screen screen, string text, string? title = null)
         {
             var model = new MessageBoxViewModel(text, title);
 
