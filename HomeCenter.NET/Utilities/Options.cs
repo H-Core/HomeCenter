e1e61d85294c26d700100f9e74d96eefe498ffe1
Konstantin S <havendv@gmail.com>
2020-01-10 14:37:16 +1000

    Fixed Options.FilePath

--- HomeCenter.NET/Utilities/Options.cs
+++ HomeCenter.NET/Utilities/Options.cs
@@ -4,7 +4,7 @@ namespace HomeCenter.NET.Utilities
 {
     public static class Options
     {
-        public static string FilePath => Assembly.GetExecutingAssembly().Location; //Application.ResourceAssembly.Location
+        public static string FilePath => Assembly.GetExecutingAssembly().Location.Replace(".dll", "") + ".exe"; //Application.ResourceAssembly.Location
         public const string ApplicationName = "HomeCenter.NET";
         public const string CompanyName = "HomeCenter.NET";
 
