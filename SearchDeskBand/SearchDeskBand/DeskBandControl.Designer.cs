1b7a1b2c1ea89f92eb85c51ec9e2b48b7b837aa7
Konstantin S <havendv@gmail.com>
2020-01-15 17:30:57 +1000

    Updated Designer.cs files

--- SearchDeskBand/SearchDeskBand/DeskBandControl.Designer.cs
+++ SearchDeskBand/SearchDeskBand/DeskBandControl.Designer.cs
@@ -32,7 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeskBandControl));
             this.Label = new System.Windows.Forms.Label();
             this.menuButton = new System.Windows.Forms.Button();
-            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.ImageList = new System.Windows.Forms.ImageList(this.components);
             this.RecordButton = new System.Windows.Forms.Button();
             this.uiButton = new System.Windows.Forms.Button();
             this.settingsButton = new System.Windows.Forms.Button();
@@ -43,9 +43,10 @@
             this.Label.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Label.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Label.ForeColor = System.Drawing.Color.Gray;
-            this.Label.Location = new System.Drawing.Point(6, 6);
+            this.Label.Location = new System.Drawing.Point(9, 9);
+            this.Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label.Name = "Label";
-            this.Label.Size = new System.Drawing.Size(304, 26);
+            this.Label.Size = new System.Drawing.Size(456, 40);
             this.Label.TabIndex = 0;
             this.Label.Text = "Enter Command Here";
             this.Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -57,24 +58,25 @@
             this.menuButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.RoyalBlue;
             this.menuButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PowderBlue;
             this.menuButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.menuButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.menuButton.ImageIndex = 1;
-            this.menuButton.ImageList = this.imageList;
-            this.menuButton.Location = new System.Drawing.Point(232, 3);
+            this.menuButton.ImageList = this.ImageList;
+            this.menuButton.Location = new System.Drawing.Point(348, 5);
+            this.menuButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.menuButton.Name = "menuButton";
-            this.menuButton.Size = new System.Drawing.Size(25, 32);
+            this.menuButton.Size = new System.Drawing.Size(38, 49);
             this.menuButton.TabIndex = 1;
             this.menuButton.UseVisualStyleBackColor = true;
             this.menuButton.Click += new System.EventHandler(this.MenuButton_Click);
             // 
-            // imageList
+            // ImageList
             // 
-            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
-            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList.Images.SetKeyName(0, "record.png");
-            this.imageList.Images.SetKeyName(1, "menu.png");
-            this.imageList.Images.SetKeyName(2, "ui.png");
-            this.imageList.Images.SetKeyName(3, "settings.png");
+            this.ImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageList.ImageStream")));
+            this.ImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImageList.Images.SetKeyName(0, "record.png");
+            this.ImageList.Images.SetKeyName(1, "menu.png");
+            this.ImageList.Images.SetKeyName(2, "ui.png");
+            this.ImageList.Images.SetKeyName(3, "settings.png");
             // 
             // RecordButton
             // 
@@ -83,12 +85,13 @@
             this.RecordButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.RoyalBlue;
             this.RecordButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PowderBlue;
             this.RecordButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.RecordButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RecordButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.RecordButton.ImageIndex = 0;
-            this.RecordButton.ImageList = this.imageList;
-            this.RecordButton.Location = new System.Drawing.Point(204, 3);
+            this.RecordButton.ImageList = this.ImageList;
+            this.RecordButton.Location = new System.Drawing.Point(306, 5);
+            this.RecordButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.RecordButton.Name = "RecordButton";
-            this.RecordButton.Size = new System.Drawing.Size(25, 32);
+            this.RecordButton.Size = new System.Drawing.Size(38, 49);
             this.RecordButton.TabIndex = 2;
             this.RecordButton.UseVisualStyleBackColor = false;
             this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
@@ -99,12 +102,13 @@
             this.uiButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.RoyalBlue;
             this.uiButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PowderBlue;
             this.uiButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.uiButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.uiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.uiButton.ImageIndex = 2;
-            this.uiButton.ImageList = this.imageList;
-            this.uiButton.Location = new System.Drawing.Point(288, 3);
+            this.uiButton.ImageList = this.ImageList;
+            this.uiButton.Location = new System.Drawing.Point(432, 5);
+            this.uiButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uiButton.Name = "uiButton";
-            this.uiButton.Size = new System.Drawing.Size(25, 32);
+            this.uiButton.Size = new System.Drawing.Size(38, 49);
             this.uiButton.TabIndex = 3;
             this.uiButton.UseVisualStyleBackColor = true;
             this.uiButton.Click += new System.EventHandler(this.UiButton_Click);
@@ -115,19 +119,20 @@
             this.settingsButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.RoyalBlue;
             this.settingsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PowderBlue;
             this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.settingsButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.settingsButton.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.settingsButton.ImageIndex = 3;
-            this.settingsButton.ImageList = this.imageList;
-            this.settingsButton.Location = new System.Drawing.Point(260, 3);
+            this.settingsButton.ImageList = this.ImageList;
+            this.settingsButton.Location = new System.Drawing.Point(390, 5);
+            this.settingsButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.settingsButton.Name = "settingsButton";
-            this.settingsButton.Size = new System.Drawing.Size(25, 32);
+            this.settingsButton.Size = new System.Drawing.Size(38, 49);
             this.settingsButton.TabIndex = 4;
             this.settingsButton.UseVisualStyleBackColor = true;
             this.settingsButton.Click += new System.EventHandler(this.SettingsButton_Click);
             // 
             // DeskBandControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -137,13 +142,14 @@
             this.Controls.Add(this.menuButton);
             this.Controls.Add(this.Label);
             this.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.MaximumSize = new System.Drawing.Size(318, 40);
-            this.MinimumSize = new System.Drawing.Size(318, 40);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(476, 60);
+            this.MinimumSize = new System.Drawing.Size(476, 60);
             this.Name = "DeskBandControl";
-            this.Padding = new System.Windows.Forms.Padding(6);
-            this.Size = new System.Drawing.Size(316, 38);
-            this.Click += new System.EventHandler(this.OnClick);
+            this.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.Size = new System.Drawing.Size(474, 58);
             this.Load += new System.EventHandler(this.DeskBandControl_Load);
+            this.Click += new System.EventHandler(this.OnClick);
             this.ResumeLayout(false);
 
         }
@@ -153,7 +159,7 @@
         private System.Windows.Forms.Label Label;
         private System.Windows.Forms.Button menuButton;
         private System.Windows.Forms.Button RecordButton;
-        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ImageList ImageList;
         private System.Windows.Forms.Button uiButton;
         private System.Windows.Forms.Button settingsButton;
     }
--- SearchDeskBand/SearchDeskBand/DeskBandWindow.Designer.cs
+++ SearchDeskBand/SearchDeskBand/DeskBandWindow.Designer.cs
@@ -45,10 +45,11 @@
             this.Panel.Controls.Add(this.TextBox);
             this.Panel.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Panel.Location = new System.Drawing.Point(0, 285);
+            this.Panel.Location = new System.Drawing.Point(0, 438);
+            this.Panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Panel.Name = "Panel";
-            this.Panel.Padding = new System.Windows.Forms.Padding(6, 7, 6, 6);
-            this.Panel.Size = new System.Drawing.Size(318, 40);
+            this.Panel.Padding = new System.Windows.Forms.Padding(9, 11, 9, 9);
+            this.Panel.Size = new System.Drawing.Size(477, 62);
             this.Panel.TabIndex = 0;
             this.Panel.Click += new System.EventHandler(this.Panel_Click);
             // 
@@ -60,9 +61,10 @@
             this.TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TextBox.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.TextBox.ForeColor = System.Drawing.Color.Navy;
-            this.TextBox.Location = new System.Drawing.Point(6, 7);
+            this.TextBox.Location = new System.Drawing.Point(9, 11);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TextBox.Name = "TextBox";
-            this.TextBox.Size = new System.Drawing.Size(306, 26);
+            this.TextBox.Size = new System.Drawing.Size(459, 39);
             this.TextBox.TabIndex = 1;
             this.TextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyUp);
             // 
@@ -72,10 +74,11 @@
             this.tabControl.Controls.Add(this.historyTabPage);
             this.tabControl.Dock = System.Windows.Forms.DockStyle.Top;
             this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabControl.Multiline = true;
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(318, 285);
+            this.tabControl.Size = new System.Drawing.Size(477, 438);
             this.tabControl.TabIndex = 1;
             // 
             // historyTabPage
@@ -83,9 +86,10 @@
             this.historyTabPage.Controls.Add(this.clearHistoryButton);
             this.historyTabPage.Controls.Add(this.historyListBox);
             this.historyTabPage.Location = new System.Drawing.Point(4, 4);
+            this.historyTabPage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.historyTabPage.Name = "historyTabPage";
-            this.historyTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.historyTabPage.Size = new System.Drawing.Size(291, 277);
+            this.historyTabPage.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.historyTabPage.Size = new System.Drawing.Size(445, 430);
             this.historyTabPage.TabIndex = 0;
             this.historyTabPage.Text = "History";
             this.historyTabPage.UseVisualStyleBackColor = true;
@@ -93,9 +97,10 @@
             // clearHistoryButton
             // 
             this.clearHistoryButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.clearHistoryButton.Location = new System.Drawing.Point(3, 243);
+            this.clearHistoryButton.Location = new System.Drawing.Point(4, 377);
+            this.clearHistoryButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.clearHistoryButton.Name = "clearHistoryButton";
-            this.clearHistoryButton.Size = new System.Drawing.Size(285, 31);
+            this.clearHistoryButton.Size = new System.Drawing.Size(437, 48);
             this.clearHistoryButton.TabIndex = 1;
             this.clearHistoryButton.Text = "Clear History";
             this.clearHistoryButton.UseVisualStyleBackColor = true;
@@ -105,21 +110,24 @@
             // 
             this.historyListBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.historyListBox.FormattingEnabled = true;
-            this.historyListBox.Location = new System.Drawing.Point(3, 3);
+            this.historyListBox.ItemHeight = 20;
+            this.historyListBox.Location = new System.Drawing.Point(4, 5);
+            this.historyListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.historyListBox.Name = "historyListBox";
-            this.historyListBox.Size = new System.Drawing.Size(285, 238);
+            this.historyListBox.Size = new System.Drawing.Size(437, 364);
             this.historyListBox.TabIndex = 0;
             this.historyListBox.DoubleClick += new System.EventHandler(this.HistoryListBox_DoubleClick);
             // 
             // DeskBandWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(318, 325);
+            this.ClientSize = new System.Drawing.Size(477, 500);
             this.ControlBox = false;
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.Panel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DeskBandWindow";
